f03fabf88cb395eb261f1a9690d42eb64a04a758
KubaTworek <kubatvorek@gmail.com>
2022-06-24 01:48:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Memory-Game
+ Memory game made for GUI classes. Using JavaFX as a gui interface.
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module pl.pjatk.memorygame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens pl.pjatk.memorygame to javafx.fxml;
+    exports pl.pjatk.memorygame;
+}
